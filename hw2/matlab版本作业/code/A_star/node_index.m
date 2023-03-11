501781febfe31a691a73a9501b0d1d4528739747
Eugene21219 <398618174@qq.com>
2023-03-11 16:07:53 +0800

    finish hw2/matlab

--- hw2/matlab版本作业/code/A_star/node_index.m
+++ hw2/matlab版本作业/code/A_star/node_index.m
@@ -1,6 +1,6 @@
 function n_index = node_index(OPEN,xval,yval)
-    %This function returns the index of the location of a node in the list
-    %OPEN
+    % This function returns the index of the location of a node in the list
+    % OPEN
     %
     %   Copyright 2009-2010 The MathWorks, Inc.
     i=1;
