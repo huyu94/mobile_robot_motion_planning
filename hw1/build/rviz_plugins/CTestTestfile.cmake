6cd52439e29864337aae35dedb5a0521e149f50b
huyu94 <398618174@qq.com>
2023-03-11 16:30:13 +0800

    finish hw1/ros

---
+++ hw1/build/rviz_plugins/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/huyu/code/mobile_robot_motion_planning/hw1/src/rviz_plugins
+# Build directory: /home/huyu/code/mobile_robot_motion_planning/hw1/build/rviz_plugins
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
